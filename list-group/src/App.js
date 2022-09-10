b73a2d5046691101758f57e2e4754a897641129f
Irina-Bondarenko <96590953+Irina-Bondarenko@users.noreply.github.com>
2022-09-10 20:04:08 +0300

    init

---
+++ list-group/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import ListGroup from "./components/List-group";
+
+function App() {
+  return (
+    <div className="App">
+      <ListGroup>
+        <p>one</p>
+        <p>two</p>
+        <p>three</p>
+        <p>four</p>
+        <p>five</p>
+      </ListGroup>
+    </div>
+  );
+}
+
+export default App;
